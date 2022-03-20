91da9dffa026bc410d2194aea781d5d6cb5e84cb
George Balch <george089@revature.net>
2022-03-20 08:44:27 -0700

    Refactor DB connection info to separate class

--- src/main/java/com/revature/utility/ConnectionUtility.java
+++ src/main/java/com/revature/utility/ConnectionUtility.java
@@ -8,15 +8,15 @@ import java.sql.SQLException;
 
 public class ConnectionUtility {
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection(DataSource dataSource) throws SQLException {
 
         DriverManager.registerDriver(new Driver());
 
-        String url = System.getenv("db_url");
-        String username = System.getenv("db_username");
-        String password = System.getenv("db_password");
-
-        return DriverManager.getConnection(url, username, password);
+        return DriverManager.getConnection(
+                dataSource.getUrl(),
+                dataSource.getUsername(),
+                dataSource.getPassword()
+        );
     }
 }
 
--- src/test/java/com/revature/dao/EmployeeDaoTest.java
+++
@@ -1,8 +0,0 @@
-package com.revature.dao;
-
-import org.junit.jupiter.api.Test;
-
-public class EmployeeDaoTest {
-    @Test
-    public void
-}
